b0e50391da5c22b6f89997623ecc7ec301aeedba
bfren <ben@bcgdesign.com>
2022-05-09 17:31:47 +0100

    Adding support for Redis 7

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="4.2.11"
+echo "Base: ${BASE_REVISION}"
+
+REDIS_VERSIONS="6 7"
+for V in ${REDIS_VERSIONS} ; do
+
+    echo "Redis ${V}"
+    ALPINE_MINOR=`cat ./${V}/ALPINE_MINOR`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        ALPINE_MINOR=${ALPINE_MINOR} \
+        BASE_REVISION=${BASE_REVISION} \
+        REDIS_MAJOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
