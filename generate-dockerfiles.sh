0ad188fa1529b40b994f237483a4da485411af56
bfren <bf@bfren.dev>
2023-12-09 11:37:34 +0000

    v5.1.0 (#130)
    Major updates
    * Updating to Redis 7.2.3 and Alpine base image 3.19
    
    Minor updates
    * Using latest base image for Redis 6

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,21 +4,21 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="5.0.5"
+BASE_VERSION="5.1.0"
 echo "Base: ${BASE_VERSION}"
 
 REDIS_VERSIONS="6 7"
 for V in ${REDIS_VERSIONS} ; do
 
     echo "Redis ${V}"
-    ALPINE_MINOR=`cat ./${V}/ALPINE_MINOR`
+    ALPINE_EDITION=`cat ./${V}/ALPINE_EDITION`
 
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
         -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
-        ALPINE_EDITION=${ALPINE_MINOR} \
+        ALPINE_EDITION=${ALPINE_EDITION} \
         BASE_VERSION=${BASE_VERSION} \
         REDIS_MAJOR=${V}
     )
