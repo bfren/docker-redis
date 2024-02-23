88347ad8a3882ae99ab707da2998f6cdf0cbe512
bfren <bf@bfren.dev>
2024-02-23 19:45:32 +0000

    v5.2.0 (#134)
    Major updates
    * Adding new test framework
    
    Minor updates
    * Using latest base images

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+IMAGE=redis
+VERSION=`cat VERSION`
+REDIS=${1:-7}
+TAG=${IMAGE}-test
+
+docker buildx build \
+    --load \
+    --build-arg BF_IMAGE=${IMAGE} \
+    --build-arg BF_VERSION=${VERSION} \
+    -f ${REDIS}/Dockerfile \
+    -t ${TAG} \
+    . \
+    && \
+    docker run --entrypoint "/usr/bin/env" ${TAG} -i nu -c "use bf test ; test"
